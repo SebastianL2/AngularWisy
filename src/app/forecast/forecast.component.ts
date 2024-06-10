df4459321df4bfc115d69b72aba0164795ead5bc
SebastianL2 <sebas16cely@gmail.com>
2024-06-10 10:23:59 -0500

    this is a commit for a secondary branch

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -113,7 +113,7 @@ constructor(private service:MasterService){}
       backgroundColor: [
         'rgb(255, 99, 132)',
         'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
+        'gray'
       ],
       hoverOffset: 4
     }]
